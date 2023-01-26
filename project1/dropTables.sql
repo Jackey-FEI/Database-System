dc7a1a66123e4af086c532760f81f0e959310eb3
FJackey-FEI <2675299845@qq.com>
2023-01-25 22:50:40 -0500

    Check and complete dropTables.sql

---
+++ project1/dropTables.sql
@@ -0,0 +1,13 @@
+DROP TABLE Friends;
+DROP TABLE User_Current_Cities;
+DROP TABLE User_Hometown_Cities;
+DROP TABLE Participants;
+DROP TABLE Tags;
+DROP TABLE Photos CASCADE CONSTRAINTS;
+DROP TABLE Albums CASCADE CONSTRAINTS;
+DROP TABLE User_Events;
+DROP TABLE Education;
+DROP TABLE Programs;
+DROP TABLE Messages;
+DROP TABLE Cities;
+DROP TABLE Users;
