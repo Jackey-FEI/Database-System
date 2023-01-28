7c1809dde64ade67ccb94dad500f4adf4adf4827
FJackey-FEI <2675299845@qq.com>
2023-01-28 00:45:03 -0500

    Finish draft of all parts without testing

--- project1/creatViews.sql
+++ project1/creatViews.sql
@@ -68,9 +68,6 @@ LEFT JOIN Institution_F
   ON Users.user_id = Institution_F.user_id;
 
 ALTER TABLE View_User_Information
-ADD PRIMARY KEY (user_id);
-
-ALTER TABLE View_User_Information
 MODIFY first_name NOT NULL;
 
 ALTER TABLE View_User_Information
@@ -101,3 +98,125 @@ INSERT INTO View_Are_Friends(user1_id,user2_id)
 SELECT user1_id,user2_id
 FROM Friends;
 
+CREATE TABLE View_Photo_Information As:
+SELECT
+    Albums.album_id,
+    Albums.owner_id,
+    Albums.cover_photo_id,
+    Albums.album_name,
+    Albums.album_created_time,
+    Albums.album_modified_time,
+    Albums.album_link,
+    Albums.album_visibility,
+    Photos.photo_id,
+    Photos.photo_caption,
+    Photos.photo_created_time,
+    Photos.photo_modified_time,
+    Photos.photo_link
+FROM Albums
+JOIN Photos
+  ON Albums.album_id = Photos.album_id;
+
+ALTER TABLE View_Photo_Information
+MODIFY album_id NOT NULL;
+
+ALTER TABLE View_Photo_Information
+MODIFY owner_id NOT NULL;
+
+ALTER TABLE View_Photo_Information
+MODIFY cover_photo_id NOT NULL;
+
+ALTER TABLE View_Photo_Information
+MODIFY album_name NOT NULL;
+
+ALTER TABLE View_Photo_Information
+MODIFY album_created_time NOT NULL;
+
+ALTER TABLE View_Photo_Information
+MODIFY album_modified_time NOT NULL;
+
+ALTER TABLE View_Photo_Information
+MODIFY album_link NOT NULL;
+    
+ALTER TABLE View_Photo_Information
+MODIFY album_visibility NOT NULL;
+
+ALTER TABLE View_Photo_Information
+MODIFY photo_id NOT NULL;
+
+ALTER TABLE View_Photo_Information
+MODIFY photo_created_time NOT NULL;
+
+ALTER TABLE View_Photo_Information
+MODIFY photo_modified_time NOT NULL;
+
+ALTER TABLE View_Photo_Information
+MODIFY photo_link NOT NULL;
+
+CREATE TABLE View_Tag_Information (
+    photo_id INTEGER NOT NULL,
+    tag_subject_id INTEGER NOT NULL,
+    tag_created_time TIMESTAMP NOT NULL,
+    tag_x_coordinate NUMBER NOT NULL,
+    tag_y_coordinate NUMBER NOT NULL,
+);
+
+INSERT INTO View_Tag_Information(photo_id, tag_subject_id, tag_created_time, tag_x_coordinate, tag_y_coordinate)
+SELECT tag_photo_id, tag_subject_id, tag_created_time, tag_x, tag_y
+FROM Tags;
+
+CREATE TABLE View_Event_Information As:
+SELECT
+    User_Events.event_id,
+    User_Events.event_creator_id,
+    User_Events.event_name,
+    User_Events.event_tagline,
+    User_Events.event_description,
+    User_Events.event_host,
+    User_Events.event_type,
+    User_Events.event_subtype,
+    User_Events.event_address,
+    Cities.city_name As event_city,
+    Cities.state_name AS event_state,
+    Cities.country_name As event_country,
+    User_Events.event_start_time,
+    User_Events.event_end_time
+FROM User_Events
+JOIN Cities
+  ON User_Events.event_city_id = Cities.city_id;
+
+ALTER TABLE View_Event_Information
+MODIFY event_id NOT NULL;
+
+ALTER TABLE View_Event_Information
+MODIFY event_creator_id NOT NULL;
+
+ALTER TABLE View_Event_Information
+MODIFY event_name NOT NULL;
+
+ALTER TABLE View_Event_Information
+MODIFY event_host NOT NULL;
+
+ALTER TABLE View_Event_Information
+MODIFY event_type NOT NULL;
+
+ALTER TABLE View_Event_Information
+MODIFY event_subtype NOT NULL;
+
+ALTER TABLE View_Event_Information
+MODIFY event_address NOT NULL;
+
+ALTER TABLE View_Event_Information
+MODIFY event_city NOT NULL;
+
+ALTER TABLE View_Event_Information
+MODIFY event_state NOT NULL;
+
+ALTER TABLE View_Event_Information
+MODIFY event_country NOT NULL;
+
+ALTER TABLE View_Event_Information
+MODIFY event_start_time NOT NULL;
+
+ALTER TABLE View_Event_Information
+MODIFY event_end_time NOT NULL;
---
+++ project1/dropViews.sql
@@ -0,0 +1,5 @@
+DROP TABLE View_Event_Information;
+DROP TABLE View_Tag_Information;
+DROP TABLE View_Photo_Information;
+DROP TABLE View_Are_Friends;
+DROP TABLE View_User_Information;
