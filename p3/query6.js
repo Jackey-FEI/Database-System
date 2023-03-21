d871331571a522a4b29b4f1d7e15cc505d7b78b3
FJackey-FEI <2675299845@qq.com>
2023-03-21 17:55:46 -0400

    Finish all queries

--- p3/query6.js
+++ p3/query6.js
@@ -6,6 +6,27 @@ function find_average_friendcount(dbname) {
     db = db.getSiblingDB(dbname);
 
     // TODO: calculate the average friend count
-
-    return 0;
+    let count = 0;
+    let num = 0;
+    let curs = db.users.aggregate([
+        {
+        $project: {
+            friend_num:  { $cond: { if: { $isArray: "$friends" }, then: { $size: "$friends" }, else: 0 } }
+        },
+    },
+     {   $group: {
+            _id: 0,
+            average: {$avg: "$friend_num"} 
+        }
+    }
+    ])
+    // db.users.find({}
+    //     ,{
+    //         _id:0,
+    //         friend_num: { $cond: { if: { $isArray: "$friends" }, then: { $size: "$friends" }, else: 0 } }
+    //     }).forEach( function(userall){
+    //         count+=userall.friend_num;
+    //         num = num +1;
+    //     });
+    return curs.next().average;
 }
--- p3/query8.js
+++ p3/query8.js
@@ -3,15 +3,25 @@
 
 let city_average_friendcount_mapper = function () {
     // TODO: Implement the map function
+    emit(this.hometown.city, {count:1, num: this.friends.length});
 };
 
 let city_average_friendcount_reducer = function (key, values) {
     // TODO: Implement the reduce function
+    reduceVal = {count:0, num:0};
+    for (var idx =0; idx< values.length; idx++)
+    {
+       reduceVal.count= reduceVal.count + 1;
+        if (values[idx].friends && values[idx].friends.length>0){
+        reduceVal.num= values[idx].num + reduceVal.num;
+       }
+    }
+    return reduceVal;
 };
 
 let city_average_friendcount_finalizer = function (key, reduceVal) {
     // We've implemented a simple forwarding finalize function. This implementation
     // is naive: it just forwards the reduceVal to the output collection.
     // TODO: Feel free to change it if needed.
-    return reduceVal;
+    return {_id:key, value: reduceVal.num/reduceVal.count};
 };
